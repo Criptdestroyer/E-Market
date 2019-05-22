1ebcd8174bfc4dac6e05f4d4954e3158fd0dd8af
Criptdestroyer <ahmademir1201@gmail.com>
2019-05-22 23:02:10 +0700

    xml done

--- app/src/main/java/com/example/e_market/ListBookAdapter.java
+++ app/src/main/java/com/example/e_market/ListBookAdapter.java
@@ -1,4 +1,54 @@
 package com.example.e_market;
 
-public class ListBookAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ListBookAdapter extends RecyclerView.Adapter<ListBookAdapter.CategoryViewHolder> {
+    private Context context;
+    private ArrayList<Book> listBook;
+
+    public ArrayList<Book> getListBook() {
+        return listBook;
+    }
+
+    public void setListBook(ArrayList<Book> listBook) {
+        this.listBook = listBook;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemRow = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_card,viewGroup,false);
+        return new CategoryViewHolder(itemRow);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder categoryViewHolder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return getListBook().size();
+    }
+
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        TextView tvDescription;
+        TextView tvPrice;
+        ImageView imgPhoto;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById();
+        }
+    }
 }
